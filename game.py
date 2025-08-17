a2f0b04a7502fb52cf1aa573daf6213a8a57f10d
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2025-08-17 20:45:12 +0300

    init

---
+++ game.py
@@ -0,0 +1,4 @@
+# Choose the number of players (2 to 4) and roll the dice.
+# If a player rolls 1, they lose automatically and their score resets to 0.
+# The player with the highest score can choose to stay or roll again.
+# The last player who has not rolled a 1 wins.
